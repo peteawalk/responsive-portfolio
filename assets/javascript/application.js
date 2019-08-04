3b551141a644815c663f7a9bccee57db485c77db
Peter Walker <25408515+peteawalk@users.noreply.github.com>
2019-08-03 21:38:11 -0500

    changed to a 1 pager

---
+++ assets/javascript/application.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    var scrollLink = $(".scroll");
+
+    // Smooth scrolling instead of page jump on navbar clicks
+    scrollLink.click(function (event) {
+        event.preventDefault();
+        $("body, html").animate({
+            scrollTop: $(this.hash).offset().top // How far away the secton is from top of page
+        }, 1000); //duration = 1000 msec
+
+    });
+
+    // Active link switching
+    $(window).scroll(function () {
+        var scrollbarLocation = $(this).scrollTop();
+
+        console.log(scrollbarLocation);
+
+        scrollLink.each(function () {
+            var sectionOffset = $(this.hash).offset().top - 20;
+
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass("active");
+                $(this).parent().siblings().removeClass("active");
+            }
+        });
+    });
+
+
+});
